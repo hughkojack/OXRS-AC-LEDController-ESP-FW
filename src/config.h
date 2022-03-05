4deff759b69d37507d5df03e08a68a7f40403f19
Austin's Creations <you@example.com>
2022-03-04 23:39:02 -0500

    fix lib deps

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* WIFI */
+const char *  WIFI_SSID             = "your-wifi-ssid";     // WiFi network name
+const char *  WIFI_PASSWORD         = "your-wifi-password"; // WiFi network password
+
+#endif
